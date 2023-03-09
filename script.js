578c33dfe94726f9b0752c4c0a6221d99e283c52
Jelena <jelena.agiejewa@gmail.com>
2023-03-09 10:38:45 +0100

    change menu-burger

---
+++ script.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector(".burger");
+const iconBurger = document.querySelector(".fa-bars");
+// const iconX = document.querySelector(".fa-xmark");
+const iconX = document.querySelector(".fa-times");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", function () {
+    iconBurger.classList.toggle("show");
+    iconX.classList.toggle("show");
+    menu.classList.toggle("show");
+})
+    
