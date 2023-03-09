6509d271a5cc7242a8d7d07d43705b76f3e4730e
Jelena <jelena.agiejewa@gmail.com>
2023-03-09 11:18:47 +0100

    update

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const burger = document.querySelector(".burger");
 const iconBurger = document.querySelector(".fa-bars");
-// const iconX = document.querySelector(".fa-xmark");
 const iconX = document.querySelector(".fa-times");
 const menu = document.querySelector(".menu");
 
 burger.addEventListener("click", function () {
-    iconBurger.classList.toggle("show");
-    iconX.classList.toggle("show");
-    menu.classList.toggle("show");
-})
-    
+  iconBurger.classList.toggle("show");
+  iconX.classList.toggle("show");
+  menu.classList.toggle("show");
+});
