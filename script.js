e63159e4711872584dd77b2749040013e10fb8c5
Jelena <jelena.agiejewa@gmail.com>
2023-03-09 12:31:15 +0100

    add JS

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ $(".burger").on("click", function () {
     $(".fas, nav").toggleClass("show");
 })
 
+// 2.
+// const burger = document.querySelector(".burger");
+// const activeElement = document.querySelectorAll(".active");
+
+// burger.addEventListener("click", function () {
+//     for (let i = 0; i < activeElement.length; i++) {
+//         activeElement[i].classList.toggle("show")
+//     }
+// })
+
+
+// 1.
 // const burger = document.querySelector(".burger");
 // const iconBurger = document.querySelector(".fa-bars");
 // const iconX = document.querySelector(".fa-times");
