eff36602f50235d8fc6f4da7c0db481bed314b9e
Jelena <jelena.agiejewa@gmail.com>
2023-03-09 11:46:10 +0100

    add jQuery

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const burger = document.querySelector(".burger");
-const iconBurger = document.querySelector(".fa-bars");
-const iconX = document.querySelector(".fa-times");
-const menu = document.querySelector(".menu");
+$(".burger").on("click", function () {
+    $(".fas, nav").toggleClass("show");
+})
 
-burger.addEventListener("click", function () {
-  iconBurger.classList.toggle("show");
-  iconX.classList.toggle("show");
-  menu.classList.toggle("show");
-});
+// const burger = document.querySelector(".burger");
+// const iconBurger = document.querySelector(".fa-bars");
+// const iconX = document.querySelector(".fa-times");
+// const menu = document.querySelector(".menu");
+
+// burger.addEventListener("click", function () {
+//   iconBurger.classList.toggle("show");
+//   iconX.classList.toggle("show");
+//   menu.classList.toggle("show");
+// });
